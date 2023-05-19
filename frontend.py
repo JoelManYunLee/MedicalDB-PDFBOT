72521927548d44f6652c1ec8dee7b8e3188bce6d
Joel Lee <joelmyleee@gmail.com>
2023-05-19 17:35:01 -0400

    Abstracted pdfloader into functions

--- frontend.py
+++ frontend.py
@@ -1,18 +1,38 @@
 import os
 from apikey import apikey
+from pdfloader import PDF_Loader
 from langchain.llms import OpenAI
 import streamlit as st
 
-os.environ['OPENAI_API_KEY'] = apikey
+def main():
+    os.environ['OPENAI_API_KEY'] = apikey
+    
+    link = "https://watermark.silverchair.com/zdc10111000s62.pdf?token=AQECAHi208BE49Ooan9kkhW_Ercy7Dm3ZL_9Cf3qfKAc485ysgAAAsQwggLABgkqhkiG9w0BBwagggKxMIICrQIBADCCAqYGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQM0QZULkXYp4HTYlAnAgEQgIICd_gL77IJyq3VMQNWw33lXqnmKEDNfBGr_49CT1N58sl1r5rX1Ga3thKvrCxwocvNt9791ayEXbxIXLip6XkhE92rLmGtW70ek6hNji1zPkz-ThUI2Az2tMYJTYT_gGEYC5exJr1hmtNcpmNvYO2-Vn0RW1rOts3ciw22VKAJyTgZkxLH0R0kHudtC27S23khx0iYzMWLT4ZoaARvyw0CjH5t3648S9O1UpyfqXoon7TR2PtvjKq7nhC8qJUHnzvVKeOAWjjy9W4XEE45yLz1pS-sMiLuL7X6Dxb2RSEdSt1_X5EGqnt-Y06QduLkgWgPqTrG_3KVEpVHRXBaxhEFUCdfLRa6C_l7kfQAuePf7oixL-1eelzXyH_OONi8W5jVOw78WmpqLVAZp0APghFErROOzPVOpBXFjyfhRwSvSaq--NCR4CaObM6qGXicrLe2aCqWL0cICybmFPoo4ENIxTz5udF2hrt0CGQ2ASxJPV4Qd37bVNFm2_5ITi3faErDQF8v6NoOMxehjAiSFsCA5MD_J-Ubia0YDnRoMuuo-0NzvTrbeiMpJEKzCnqoqym6m4mu3rawVWz3DiKWSgiR9ZAEpW3JdMxNCJdmetTzQH94m68vjDkxm-TCv1VD_ZrE8FWhczn9N76bCXj51WM84_Xw54KNniZ-rV3wTfp_MuxY6xKLZiAKBAscEgJ_jbPaKVe17UqGkN1O_c001QLs96ojh0BwXuARM57NBh8YTiBo1SJBGtiktQfPEEFj4Mo15hx0BVtD0tE3B1PM6AwS1MVsIyogBYtUDB3O6To0RmKhcmW_kJddF0Zi7ghY5kck9blAwKvcw_w"
 
-# Display the page title and the text box for the user to ask the question
-st.title('🦜 Search and query academic medical papers ')
-prompt = st.text_input("What medical topic would you like to know about?")
+    # Display the page title and the text box for the user to ask the question
+    st.title('🦜 Search and query academic medical papers ')
 
-llm = OpenAI(temperature = 0.9)
+    #Take in some prompt from the user
+    # TODO: Create some context for the bot so that it knows what it's purpose is
+    # -Add additional agents and tools that can be accessed beyond the chatgpt API training
+    # -Find some way to navigate to the PDF version of the database/paper, then feed that url to the pdf loader
+    prompt = st.text_input("What medical topic would you like to know about?")
 
-if prompt:
-    response = llm(prompt)
-    st.write(response)
+    vectorized_doc = PDF_Loader.loadPDF(url=link)
+
+    PDF_Loader.queryPDF(prompt, vectorized_doc)
+    
+    llm = OpenAI(temperature = 0.9)
+
+    if prompt:
+
+        response = llm(prompt)
+        st.write(response)
+
+
+if __name__ == '__main__':
+    main()
+
+
 
 
--- pdfloader.py
+++ pdfloader.py
@@ -14,26 +14,31 @@ from langchain.llms import OpenAI
 os.environ['OPENAI_API_KEY'] = apikey
 model_id = "gpt-3.5-turbo"
 
-# TODO: Temporarily load a PDF from some archive, 
-# need to change this to the pdf that is retrieved from pubmed API
-loader = OnlinePDFLoader("https://arxiv.org/pdf/2302.03803.pdf")
-pdf_data = loader.load()
+class PDF_Loader:
+    def loadPDF(url):
+        # TODO: Temporarily load a PDF from some archive, 
+        # need to change this to the pdf that is retrieved from pubmed API
+        # loader = OnlinePDFLoader("https://arxiv.org/pdf/2302.03803.pdf")
+        loader = OnlinePDFLoader(url)
+        pdf_data = loader.load()
 
-# Split up the document to avoid token threshold
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 2000,
-    chunk_overlap = 200,
-)
+        # Split up the document to avoid token threshold
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 2000,
+            chunk_overlap = 200,
+        )
 
-# Create OpenAIEmbeddings and FAISS objects. Vectorize the chunks created above and save.
-documents = text_splitter.split_documents(pdf_data)
-embeddings = OpenAIEmbeddings()
-docsearch = FAISS.from_documents(documents, embeddings)
+        # Create OpenAIEmbeddings and FAISS objects. Vectorize the chunks created above and save.
+        documents = text_splitter.split_documents(pdf_data)
+        embeddings = OpenAIEmbeddings()
+        docsearch = FAISS.from_documents(documents, embeddings)
 
-my_llm = OpenAI(model_name= model_id)
+        return docsearch
 
-chain = load_qa_chain(my_llm, chain_type="stuff")
+    def queryPDF(query, docsearch):
+        chain = load_qa_chain(OpenAI(), chain_type="stuff")
+        # pass some query to the API to interact with the PDF
+        docs = docsearch.similarity_search(query)
+        chain.run(input_documents=docs, question=query)
 
-query = "who are the authors of this article?"
-docs = docsearch.similarity_search(query)
-chain.run(input_documents=docs, question=query)
+
